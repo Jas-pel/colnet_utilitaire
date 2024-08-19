86b4382090db8fee22196ac7fa8d66c96a1881af
Jasmin <peleltierjasmin7@gmail.com>
2024-08-19 11:28:35 -0400

    Modif fonc 7

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             case 6 : 
                 supprimer_profil()
             case 7 : 
-                send_message(user=user, message="Salut", object="Test", number_of_message=30, search_manually=False)
+                send_message(user=user, message="Salut", object="Test", number_of_message=50, search_manually=False)
             case 8 : break
 
         _ = input(Fore.GREEN + "\nTapez une touche pour continuer" + Style.RESET_ALL)
